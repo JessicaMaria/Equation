97d8e2fab424e57b15c394a9cc27d3299a5445a5
Isaac Medina <MIsaacMedina@gmail.com>
2015-11-17 09:52:01 +0000

    First test.

---
+++ test/persistance/loader_.java
@@ -0,0 +1,8 @@
+package persistance;
+
+public class loader_ {
+    public void should_do_something() {
+        XLoader xLoader = new XLoader();
+        assertEquals(90, xLoader.getSomething());
+    }
+}
