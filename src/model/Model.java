57ee4eb08dc7b460567aa6773ac6b1ca0b9f681a
Rubén David <rubendavidbm@gmail.com>
2015-11-17 11:37:10 +0000

    error ruben

--- src/model/Model.java
+++ src/model/Model.java
@@ -3,6 +3,7 @@ package model;
 /**
  * Created by usuario on 17/11/2015.
  */
+//Ruben David Brito Mendez
 public class Model {
     public static void main(String[] args) {
         System.out.println("Aqui en el modelo");
