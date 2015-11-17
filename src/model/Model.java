0546e4ebd02c53183c58b500205ce98049c2cee8
Dinamic Duo <crp93.2@gmail.com>
2015-11-17 09:39:25 -0100

    message model

--- src/model/Model.java
+++ src/model/Model.java
@@ -4,5 +4,7 @@ package model;
  * Created by usuario on 17/11/2015.
  */
 public class Model {
-    
+    public static void main(String[] args) {
+        System.out.println("Aqui en el modelo");
+    }
 }
